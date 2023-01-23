a28da4a91c24696a360e7a08feecf7b044e445ce
ja ma ma <jmcmahon24@dtechhs.org>
2023-01-23 10:22:36 -0600

    gun fixed

--- Assets/scripts/gunScript.cs
+++ Assets/scripts/gunScript.cs
@@ -9,9 +9,13 @@ public class gunScript : MonoBehaviour
     public int bulletSpeed = 10;
     public int reloadTime = 10;
     public GameObject bullet;
+    private Quaternion rotation;
+    private void Update() {
+        rotation = transform.rotation * Quaternion.AngleAxis(90, Vector3.right);
+    }
     public void shoot() {
         Debug.Log("BANG!!!");
-        Instantiate(bullet, transform, true);
+        Instantiate(bullet, transform.position, rotation);
     }
 
     public void relode() {
