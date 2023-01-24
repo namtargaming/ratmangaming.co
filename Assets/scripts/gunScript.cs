fba40450739d318ae744047c95f0fd6f1f2c4df1
ja ma ma <jmcmahon24@dtechhs.org>
2023-01-24 10:22:03 -0600

    left gun working

--- Assets/scripts/gunScript.cs
+++ Assets/scripts/gunScript.cs
@@ -8,12 +8,36 @@ public class gunScript : MonoBehaviour
 {
     public int bulletSpeed = 10;
     public int reloadTime = 10;
+    public bool sideLT_RF = true;
     public GameObject bullet;
     public GameObject bulletCasing;
     private Quaternion rotation;
+    [SerializeField]
+    private InputActionReference shootR, shootL, reloadR, reloadL;
+
     private void Update() {
         rotation = transform.rotation * Quaternion.AngleAxis(90, Vector3.right);
     }
+
+    private void OnEnable() {
+        shootL.action.performed += ShootGun;
+        shootR.action.performed += ShootGun;
+    }
+    
+    private void OnDisable() {
+        shootL.action.performed -= ShootGun;
+        shootR.action.performed -= ShootGun;
+    }
+
+    private void ShootGun(InputAction.CallbackContext obj){
+        if(sideLT_RF == false){
+            shoot();
+        }
+        if(sideLT_RF == true){
+            shoot2();
+        }
+    }
+
     public void shoot() {
     Instantiate(bullet, transform.position, rotation);
     Instantiate(bulletCasing, transform.position, rotation);
@@ -24,13 +48,13 @@ public class gunScript : MonoBehaviour
 
     }
 
-    public void shootL() {
-    Debug.Log("BANG!!!");
-    Instantiate(bullet, transform.position, rotation);
-    Instantiate(bulletCasing, transform.position, rotation);
-    }
+   public void shoot2() {
+   Debug.Log("BANG!!!");
+   Instantiate(bullet, transform.position, rotation);
+   Instantiate(bulletCasing, transform.position, rotation);
+   }
 
-    public void relodeL () {
+   public void relodeL () {
 
-    }
+   }
 }
