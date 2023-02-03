b884a95f0b65d54738e406f930aab771f27eb4ba
Mchero7 <68868620+Mchero7@users.noreply.github.com>
2023-02-02 23:22:43 -0800

    please fix

--- Assets/scripts/enime.cs
+++ Assets/scripts/enime.cs
@@ -8,8 +8,9 @@ public class enime : MonoBehaviour
 {
     public Transform player;
     public int topSpeed = 100;
+    public int followSpeed = 100;
     public Rigidbody Enime;       
-    private Vector3 velosity;
+    private float velosity;
     // Start is called before the first frame update
 
         private void Awake()
@@ -19,14 +20,21 @@ public class enime : MonoBehaviour
     void Start()
     {
     //    Enime = GetComponent<Rigidbody>();
+        velosity = Enime.velocity.x * Enime.velocity.y * Enime.velocity.z / 3; 
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.LookAt(player.position);
-//        if(velosity >= new Vector3(topSpeed,topSpeed,topSpeed))
-        Enime.AddForce(transform.rotation * new Vector3(0,0,topSpeed));
+        //Debug.Log(velosity);
+        //Debug.Log(Enime.velocity.x);
+        //Debug.Log(Enime.velocity.z);
+        //Debug.Log(Enime.velocity.y);
+//
+        //transform.LookAt(player.position);
+        //if(velosity <= topSpeed){
+        //Enime.AddForce(transform.rotation * new Vector3(0,0,followSpeed));
+        }
     }
     private void OnCollisionEnter(Collision other) {
         if(other.gameObject.tag == ("playerBullet")){
