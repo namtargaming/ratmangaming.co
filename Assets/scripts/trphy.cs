8fbaf60720e2adf340c2dd4c29eb898e58ecc647
ja ma ma <jmcmahon24@dtechhs.org>
2023-04-11 11:22:07 -0600

    added trophy and end sean

---
+++ Assets/scripts/trphy.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+public class trphy : MonoBehaviour
+{   
+
+    [SerializeField]
+    // Start is called before the first frame update
+
+   void OnCollisionEnter(Collision collision){
+     if(collision.gameObject.tag == ("Player"))
+     {
+        SceneManager.LoadScene("end"); 
+     }
+    }
+
+
+}
